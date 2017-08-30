47c02d7ec9744cf74e9d6831dc13a2572b4614d0
xuefuwu <alan.wu@fbvalve.com>
2017-08-30 08:11:10 +0800

    json post

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,9 @@ export class AppConfig {
     private domain:string;
     private api:string;
     constructor() {
-        //this.domain = "http://192.168.0.102:8100";
-        this.domain = "http://oa.wzmzzj.gov.cn/www/";
-        this.api = "http://oa.wzmzzj.gov.cn/weboa";
+        this.domain = "http://localhost:8100";
+        //this.domain = "http://oa.wzmzzj.gov.cn/www/";
+        this.api = "http://www.fbvinc.com:8137/weboa";
     }
     get Domain(){
         return this.domain;
